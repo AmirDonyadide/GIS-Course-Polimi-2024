aef1c0f20f66342dc25757f780e0bde0d6ce335a
AmirDonyadide <donyadideganamir@gmail.com>
2024-06-10 18:37:37 +0200

    release

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,12 +1,15 @@
 import 'ol/ol.css';
 import 'ol-layerswitcher/dist/ol-layerswitcher.css';
-import { Map, View} from 'ol';
-import { Tile, Image, Group,} from 'ol/layer';
+import { Map, View, Overlay } from 'ol';
+import { Tile, Image, Group, Vector } from 'ol/layer';
 import { OSM, ImageWMS, BingMaps, StadiaMaps } from 'ol/source';
+import VectorSource from 'ol/source/Vector';
+import { GeoJSON } from 'ol/format';
 import { fromLonLat } from 'ol/proj';
 import { ScaleLine, FullScreen, MousePosition } from 'ol/control';
 import LayerSwitcher from 'ol-layerswitcher';
 import { createStringXY } from 'ol/coordinate';
+import { Style, Stroke } from 'ol/style';
 
 let osm = new Tile({
     title: "Open Street Map",
